15d7325b899fe394859f212c1ecede69e2a44469
Régis de Oliveira Lima <regis.lima@fitbank.com.br>
2021-09-27 16:53:18 -0300

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton jogador1 = Singleton.GetInstancia;
+            jogador1.Mensagem("Jogador 1: A bola está comigo no meio de campo");
+
+            Singleton jogador2 = Singleton.GetInstancia;
+            jogador2.Mensagem("Jogador 2: recebeu a bola");
+
+            Singleton jogador3 = Singleton.GetInstancia;
+            jogador3.Mensagem("Jogador 3: recebeu o lançamento na grande área");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Bola em jogo");
+                }
+
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
